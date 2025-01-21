2c1e233d3fd39bb0c22f22a10149c7d4e1473371
Tevyn <tevyn.payne@gmail.com>
2025-01-21 16:06:40 -0600

    fixing the accessor for the chance of rain

--- weather.py
+++ weather.py
@@ -17,14 +17,14 @@ print(soup.prettify())
 
 # Filter for the required data
 current_temp = soup.find("span", class_="CurrentConditions--tempValue--zUBSz")  # Updated class name
-chances_rain = soup.find("div", class_="Column--precip--YkErk")  # Updated class name
+chances_rain = soup.find("span", class_="Accessibility--visuallyHidden--n+vd9").next_sibling  # Updated class name
 
 #Extracting the text from the found elements
 temp = current_temp.text if current_temp else "N/A" #Get the first element's text
 temp_rain = chances_rain.text if chances_rain else "N/A" #Get the first element's text
 
 #prepare the result message
-result = f"Current Temperature in Euless, TX: {temp}\nChance  fo rain: {temp_rain}"
+result = f"Current Temperature in Euless, TX: {temp}\nChance of rain: {temp_rain}"
 
 #show the toast notification
 n.show_toast("Weather update", result, duration = 10)
